e40111aa4152a969b47fe57356873496b2455473
Dzmitry Sadouski <Dzmitry.Sadouski@itechart-group.com>
2020-04-30 13:14:48 +0300

    addToProductCard test finished

--- cypress/tests/addProductToCard.js
+++ cypress/tests/addProductToCard.js
@@ -4,34 +4,44 @@ import checkOutPage from "../page-object/checkOutPage";
 import commonPage from "../page-object/commonPage"
 
 context('ProductPage', () => {
-    beforeEach(() => {
-        cy.fixture('product').then(productFix =>{
-            this.product = productFix;
-            cy.log('Open page with products');
-            MainPage.open();
-            MainPage.openProduct(this.product.product.product_1.name);
-            productPage.getProductPrice().then( price => {
-                cy.wrap(price).as('productPrice')
+    describe('Product Data', () => {
 
-            });
+        let i = 0;
 
-            productPage.click_button_buy();
-            const projectObj = productPage.addProductToBasket();
-            projectObj.then(maps => cy.wrap(maps.get('price')).as('newPrice'));
-            projectObj.then(maps => cy.wrap(maps.get('name')).as('productName'))
-        })
+        beforeEach(() => {
+            cy.fixture('product').then(productFix => {
+                    if (i === 0){
+                        let currentProduct = productFix.product[i].name;
+                        cy.log(currentProduct);
+                        i++
+                    }
+                    else if (i===1){
+                        let currentProduct = productFix.product[i].name;
+                        cy.log(currentProduct);
+                    }
+                    cy.log('Open page with products');
+                    MainPage.open();
+                    MainPage.openProduct(currentProduct);
+                    productPage.getProductPrice().then(price => {
+                        cy.wrap(price).as('productPrice')
+                    });
 
+                    productPage.click_button_buy();
+                    const projectObj = productPage.addProductToBasket();
+                    projectObj.then(maps => cy.wrap(maps.get('price')).as('newPrice'));
+                    projectObj.then(maps => cy.wrap(maps.get('name')).as('productName'))
+                // }
+
+            })
+        });
+
+        it('Add product to the box', function () {
+            const finalPrice = commonPage.getFinalPrice(this.newPrice, this.productPrice);
+            checkOutPage.checkProductPrice(finalPrice);
+            checkOutPage.checkProductName(this.currentProduct, this.productName);
+            checkOutPage.checkProductQuantity(1);
+            checkOutPage.removeProduct();
+            checkOutPage.checkProductListContainItems(0);
+        });
+        });
     });
-});
-
-
-it('Add product to the box', function (){
-    const finalPrice = commonPage.getFinalPrice(this.newPrice, this.productPrice);
-    checkOutPage.checkProductPrice(finalPrice);
-    checkOutPage.checkProductName(this.product.product.product_1.name, this.productName);
-    checkOutPage.checkProductQuantity(1);
-    checkOutPage.removeProduct();
-    checkOutPage.checkProductListContainItems(0);
-
-});
-
